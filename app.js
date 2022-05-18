4fda1f79fc5ca1f46b956d9b720f74338dd7ddd9
Reyner Veliz <reynerv@yahoo.com>
2022-05-18 06:51:34 -0600

    Productos Create and New

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Producto = require("./models/producto");
 const path = require("path");
+const { render } = require("express/lib/response");
 const PORT = 3000;
 
 mongoose.connect("mongodb://localhost:27017/productos-total");
@@ -16,6 +17,8 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded( {extended: true }));
+
 app.get("/", (req, res) => {
     res.render("home");
 });
@@ -23,6 +26,18 @@ app.get("/", (req, res) => {
 app.get("/productos", async (req, res) => {
     const productos = await Producto.find({});
     res.render("productos/index", { productos });
+});
+
+app.get("/productos/new", (req, res) => {
+    res.render("productos/new");
+});
+
+app.post("/productos", async (req, res) => {
+    // res.send(req.body.producto);
+    // {"nombre":"Formol","precio":"23"}
+    const producto = new Producto(req.body.producto);
+    await producto.save();
+    res.redirect(`/productos/${producto._id}`);
 });
 
 app.get("/productos/:id", async (req, res) => {
