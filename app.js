e0678571dcb9577dd9fdc8bb64c39c8356da1e8d
Reyner Veliz <reynerv@yahoo.com>
2022-05-18 07:45:01 -0600

    Add ejs-mate tool for layouts

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 const Producto = require("./models/producto");
 const path = require("path");
 const { render } = require("express/lib/response");
@@ -15,6 +16,7 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
