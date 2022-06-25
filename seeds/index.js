44fa24b379d5893d6677948f8ef452d8e27518fd
Reyner Veliz <reynerv@yahoo.com>
2022-06-25 10:45:19 -0600

    Setup for Heroku and Mongodb

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 const Producto = require("../models/producto");
 
-mongoose.connect("mongodb://localhost:27017/productos-total");
+// const dbURL = mongodb://localhost:27017/productos-total
+const dbURL = "mongodb+srv://andres:dPmEolcIaCJcekG3@cluster0.1wqjv.mongodb.net/?retryWrites=true&w=majority"
+console.log(dbURL)
+mongoose.connect(dbURL);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
@@ -13,7 +16,7 @@ const unidades = ["galón", "litro", "botella", "caneca"]
 
 const seedDB = async () => {
     await Producto.deleteMany({});
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 7; i++) {
         const randNum = Math.floor(Math.random() * 1000) + 200;
         const prod = new Producto({
             nombre: `Producto ${i + 1}`,
@@ -31,5 +34,5 @@ const seedDB = async () => {
 //Seed DB
 seedDB().then(() => {
     mongoose.connection.close();
-})
+});
 
