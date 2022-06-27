e134bc2734a8c92c082fdecf3e6081a1425c9f47
Reyner Veliz <reynerv@yahoo.com>
2022-06-27 15:41:49 -0600

    Fix join.string() error @ app.js line 56

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.post("/productos", catchAsync(async (req, res, next) => {
         producto: Joi.object({
             nombre: Joi.string().required(),
             precio: Joi.number().required().min(0),
-            unidad: join.string().required(),
+            unidad: Joi.string().required(),
             desc: Joi.string().required(),
             imagen: Joi.string().required()
         }).required()
