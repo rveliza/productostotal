0e68d038b969727611398970beb952e0b05e7405
Reyner Veliz <36872223+rveliza@users.noreply.github.com>
2022-06-29 09:17:06 -0600

    Configure Passport

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 const port = process.env.PORT || 4000;
 
 const productos = require('./routes/productos');
@@ -45,11 +48,23 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use( (req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
+});
+
+app.get("/fakeUser", async (req, res) => {
+    const user = new User({ email: "rey@gmail.com", username: "rveliz", });
+    const newUser = await User.register(user, 'chicken');
+    res.send(newUser);
 });
 
 app.use('/productos', productos);
