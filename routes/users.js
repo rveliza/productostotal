e9b00d17bb9c57cb618309dc8a311c836397e191
Reyner Veliz <36872223+rveliza@users.noreply.github.com>
2022-06-30 09:49:10 -0600

    request catchAsync middleware in users.js

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
 router.get('/register', (req, res) => {
