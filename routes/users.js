334cb2ef5ee989f145fda13673e627bf05958ae7
Reyner Veliz <36872223+rveliza@users.noreply.github.com>
2022-06-30 09:45:52 -0600

    Register route logic

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,27 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register', async (req, res) => {
-    res.send(req.body);
-});
+// router.post('/register', catchAsync(async (req, res) => {
+//     // res.send(req.body);
+//     const { email, username, password } = req.body;
+//     const user = new User({ email, username });
+//     const registeredUser = await User.register(user, password);
+//     console.log(registeredUser);
+//     req.flash('success', 'Bienvenidos!');
+//     res.redirect('/campgrounds');
+// }));
+
+router.post('/register', catchAsync(async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.flash('success', '¡Bienvenido a Productos Total!');
+        res.redirect('/productos');
+    } catch (e) {
+        req.flash("error", e.message);
+        res.redirect("register");
+    }
+}));
 
 module.exports = router;
