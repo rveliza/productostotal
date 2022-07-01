002304fd3f69a4d55929313c232d7bb2b7a877bf
Reyner Veliz <36872223+rveliza@users.noreply.github.com>
2022-06-30 20:02:09 -0600

    Fix Register Route

--- routes/users.js
+++ routes/users.js
@@ -14,8 +14,11 @@ router.post('/register', catchAsync(async (req, res) => {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
-        req.flash('success', '¡Bienvenido a Productos Total!');
-        res.redirect('/productos');
+        req.login(registeredUser, err => {
+            if (err) return next(err)
+            req.flash('success', '¡Bienvenido a Productos Total!');
+            res.redirect('/productos');
+        });
     } catch (e) {
         req.flash("error", e.message);
         res.redirect("register");
