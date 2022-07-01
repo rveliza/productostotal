80132221c20c05c3a4cbe88988f528a4f64728e8
Reyner Veliz <36872223+rveliza@users.noreply.github.com>
2022-06-30 19:58:48 -0600

    Add currentUserHelper

--- middleware.js
+++ middleware.js
@@ -1,4 +1,11 @@
 module.exports.isLoggedIn = (req, res, next) => {
+    // console.log("REQ USER...", req.user);
+    // REQ USER... {
+    //     _id: new ObjectId("629e21386f488344b393eb8c"),
+    //     email: 'reynerv@yahoo.com',
+    //     username: 'rveliz',
+    //     __v: 0
+    //   }
     if (!req.isAuthenticated()) {
         req.flash('error', '¡Debes ingresar primero!');
         return res.redirect('/login');
