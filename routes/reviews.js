885dbd3919e4e567392925d4eb7b32876dd5333d
Reyner Veliz <36872223+rveliza@users.noreply.github.com>
2022-07-01 10:06:16 -0600

    Add validateReview const to reviews.js

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router( {mergeParams: true} );
 const catchAsync = require("../utils/catchAsync");
+const { validateReview } = require("../middleware");
 const Producto = require("../models/producto");
 const Review = require('../models/review');
 
