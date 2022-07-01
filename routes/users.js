2824e51583b4ad19ec081ae28f5ed17858bd7685
Reyner Veliz <36872223+rveliza@users.noreply.github.com>
2022-06-30 18:29:55 -0600

    Login Routes

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
@@ -7,15 +8,6 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-// router.post('/register', catchAsync(async (req, res) => {
-//     // res.send(req.body);
-//     const { email, username, password } = req.body;
-//     const user = new User({ email, username });
-//     const registeredUser = await User.register(user, password);
-//     console.log(registeredUser);
-//     req.flash('success', 'Bienvenidos!');
-//     res.redirect('/campgrounds');
-// }));
 
 router.post('/register', catchAsync(async (req, res) => {
     try {
@@ -29,5 +21,16 @@ router.post('/register', catchAsync(async (req, res) => {
         res.redirect("register");
     }
 }));
+
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', '¡Gracias por regresar!');
+    res.redirect('/productos');
+});
 
 module.exports = router;
