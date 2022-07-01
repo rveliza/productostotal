5a8dcb3df48074e2ae8aea58a430a36a1fb98bd8
Reyner Veliz <36872223+rveliza@users.noreply.github.com>
2022-07-01 12:09:21 -0600

    Add Users Controller

--- routes/users.js
+++ routes/users.js
@@ -2,46 +2,19 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
-const User = require('../models/user');
+const users = require('../controllers/users');
 
-router.get('/register', (req, res) => {
-    res.render('users/register');
-});
+router.get('/register', users.renderRegister);
 
 
-router.post('/register', catchAsync(async (req, res) => {
-    try {
-        const { email, username, password } = req.body;
-        const user = new User({ email, username });
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if (err) return next(err)
-            req.flash('success', '¡Bienvenido a Productos Total!');
-            res.redirect('/productos');
-        });
-    } catch (e) {
-        req.flash("error", e.message);
-        res.redirect("register");
-    }
-}));
+router.post('/register', catchAsync(users.register));
 
 
-router.get('/login', (req, res) => {
-    res.render('users/login');
-});
+router.get('/login', users.renderLogin);
 
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-    req.flash('success', '¡Gracias por regresar!');
-    const redirectUrl = req.session.returnTo || '/productos';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-});
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
 
-router.get('/logout', (req, res) => {
-    req.logout();
-    req.flash('success', " ¡Adiós!");
-    res.redirect('/productos');
-});
+router.get('/logout', users.logout);
 
 module.exports = router;
