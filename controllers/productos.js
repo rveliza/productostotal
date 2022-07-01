ed062f8cab54c56927366c2ab7472388cea739cf
Reyner Veliz <36872223+rveliza@users.noreply.github.com>
2022-07-01 11:51:26 -0600

    Add Productos Controller

---
+++ controllers/productos.js
@@ -0,0 +1,55 @@
+const Producto = require('../models/producto');
+
+module.exports.index = async (req, res) => {
+    const productos = await Producto.find({});
+    res.render("productos/index", { productos });
+}
+
+module.exports.renderNewForm = (req, res) => {
+    res.render("productos/new");
+}
+
+module.exports.createProducto = async (req, res, next) => {
+    const producto = new Producto(req.body.producto);
+    producto.author = req.user._id;
+    await producto.save();
+    req.flash("success", "¡Producto creado con éxito!");
+    res.redirect(`/productos/${producto._id}`);
+}
+
+module.exports.showProducto = async (req, res) => {
+    const producto = await Producto.findById(req.params.id).populate({
+        path: "reviews",
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
+    if (!producto) {
+        req.flash('error', '!No se pudo encontrar ese producto¡');
+        return res.redirect('/productos');
+    }
+    res.render("productos/show", { producto });
+}
+
+module.exports.renderEditForm = async (req, res) => {
+    const producto = await Producto.findById(req.params.id);
+    if (!producto) {
+        req.flash('error', '!No se pudo encontrar ese producto¡');
+        return res.redirect('/productos');
+    }
+    res.render("productos/edit", { producto });
+}
+
+module.exports.updateProducto = async (req, res) => {
+    const { id } = req.params;
+    const producto = await Producto.findById(id, { ...req.body.campground });
+    req.flash('success', '¡Producto actualizado con éxito!')
+    res.redirect(`/productos/${producto._id}`);
+}
+
+module.exports.deleteProducto = async (req, res) => {
+    const { id } = req.params;
+    await Producto.findByIdAndDelete(id);
+    req.flash('success', "¡Producto eliminado con éxito!");
+    res.redirect("/productos");
+}
--- routes/productos.js
+++ routes/productos.js
@@ -1,62 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const productos = require('../controllers/productos');
 const catchAsync = require("../utils/catchAsync");
-const Producto = require("../models/producto");
 const { isLoggedIn, isAuthor, validateProducto } = require('../middleware');
 
 
-router.get("/", catchAsync(async (req, res) => {
-    const productos = await Producto.find({});
-    res.render("productos/index", { productos });
-}));
+router.get("/", catchAsync(productos.index));
 
-router.get("/new", isLoggedIn, (req, res) => {
-    res.render("productos/new");
-});
+router.get("/new", isLoggedIn, productos.renderNewForm);
 
-router.post("/", isLoggedIn, validateProducto, catchAsync(async (req, res, next) => {
-    const producto = new Producto(req.body.producto);
-    producto.author = req.user._id;
-    await producto.save();
-    req.flash("success", "¡Producto creado con éxito!");
-    res.redirect(`/productos/${producto._id}`);
-}));
+router.post("/", isLoggedIn, validateProducto, catchAsync(productos.createProducto));
 
-router.get("/:id", catchAsync(async (req, res) => {
-    const producto = await Producto.findById(req.params.id).populate({
-        path: "reviews",
-        populate: {
-            path: 'author'
-        }
-    }).populate('author');
-    if (!producto) {
-        req.flash('error', '!No se pudo encontrar ese producto¡');
-        return res.redirect('/productos');
-    }
-    res.render("productos/show", { producto });
-}));
+router.get("/:id", catchAsync(productos.showProducto));
 
-router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-    const producto = await Producto.findById(req.params.id);
-    if (!producto) {
-        req.flash('error', '!No se pudo encontrar ese producto¡');
-        return res.redirect('/productos');
-    }
-    res.render("productos/edit", { producto });
-}));
+router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(productos.renderEditForm));
 
-router.put("/:id", isLoggedIn, isAuthor, validateProducto, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const producto = await Producto.findById(id, { ...req.body.campground });
-    req.flash('success', '¡Producto actualizado con éxito!')
-    res.redirect(`/productos/${producto._id}`);
-}));
+router.put("/:id", isLoggedIn, isAuthor, validateProducto, catchAsync(productos.updateProducto));
 
-router.delete("/:id", isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Producto.findByIdAndDelete(id);
-    req.flash('success', "¡Producto eliminado con éxito!");
-    res.redirect("/productos");
-}));
+router.delete("/:id", isLoggedIn, isAuthor, catchAsync(productos.deleteProducto));
 
 module.exports = router;
