f0fb326a0733071d982230a67363d6622e8d9a0f
Reyner Veliz <36872223+rveliza@users.noreply.github.com>
2022-07-04 23:31:06 -0600

    Add Thumbnail Virtual Property

--- models/producto.js
+++ models/producto.js
@@ -1,18 +1,23 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const Review = require('./review')
+const Review = require('./review');
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
 
 const ProductoSchema = new Schema({
     nombre: String,
     precio: Number,
     unidad: String,
     desc: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     author: {
         type: Schema.Types.ObjectId,
         ref: 'User'
